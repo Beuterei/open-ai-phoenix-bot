934359771a38195910c7da8d21bb1e92852f0858
Luis Beu <me@luisbeu.de>
2022-06-25 02:03:14 +0200

    init commit

---
+++ src/clients/openai.client.ts
@@ -0,0 +1,30 @@
+import Client, { CompletionOpts } from 'openai-api';
+
+const baseOptions: CompletionOpts = {
+    engine: 'davinci',
+    maxTokens: 2000,
+    temperature: 0.9,
+    topP: 1,
+    presencePenalty: 0,
+    frequencyPenalty: 0,
+    bestOf: 1,
+    n: 1,
+    stream: false,
+};
+
+export class OpenAI {
+    private client: Client;
+
+    constructor() {
+        this.client = new Client(process.env.OPENAI_API_KEY || '');
+    }
+
+    async complete(prompt: string) {
+        const result = await this.client.complete({
+            ...baseOptions,
+            prompt,
+        });
+
+        return result.data;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { CronJob } from 'cron';
+import { DiscordBot } from './clients/discordBot.client';
+
+(async () => {
+    const discordClient = new DiscordBot();
+    await discordClient.init();
+
+    console.log('Initialized!');
+
+    if (process.env.BOT_DAILY_GREENTEXT_CRON)
+        new CronJob(process.env.BOT_DAILY_GREENTEXT_CRON, () =>
+            discordClient.dailyGreentext(),
+        ).start();
+})();
