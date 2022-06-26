89c747d2569b875a08bf3ac08b13716f83773576
Luis Beu <me@luisbeu.de>
2022-06-26 14:16:01 +0200

    fix ping

--- src/commands/ping.command.ts
+++ src/commands/ping.command.ts
@@ -3,7 +3,7 @@ import { registerCommand } from '../util/discord.helper';
 
 export = registerCommand(
     new SlashCommandBuilder()
-        .setName('Ping')
+        .setName('ping')
         .setDescription('Replies with pong'),
     async (interaction) => {
         await interaction.reply({ content: 'Pong!', ephemeral: true })
